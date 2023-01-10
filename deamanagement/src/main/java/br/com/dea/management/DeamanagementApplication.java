20ac1ce5fa04a45b6018ca59a983a8d02960b707
Pietro Pugliesi <pietropugliesi@icloud.com>
2023-01-10 11:26:26 -0300

    initial commit

---
+++ deamanagement/src/main/java/br/com/dea/management/DeamanagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.dea.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeamanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeamanagementApplication.class, args);
+	}
+
+}
---
+++ deamanagement/src/test/java/br/com/dea/management/DeamanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dea.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeamanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

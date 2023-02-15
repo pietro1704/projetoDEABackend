3f6ca571ac45c298cefe0fc2563529f0b7279e48
Pietro Pugliesi <pietropugliesi@icloud.com>
2023-02-15 15:36:57 -0300

    semana4: add testes

---
+++ deamanagement/src/test/java/br/com/dea/management/student/StudentGetAllTests.java
@@ -0,0 +1,142 @@
+package br.com.dea.management.student;
+
+import br.com.dea.management.student.domain.Student;
+import br.com.dea.management.student.repository.StudentRepository;
+import br.com.dea.management.user.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Slf4j
+public class StudentGetAllTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        log.info("Before each test in " + StudentGetAllTests.class.getSimpleName());
+    }
+
+    @BeforeAll
+    void beforeSuiteTest() {
+        log.info("Before all tests in " + StudentGetAllTests.class.getSimpleName());
+    }
+
+    @Test
+    void whenRequestingStudentList_thenReturnListOfStudentPaginatedSuccessfully() throws Exception {
+        this.studentRepository.deleteAll();
+        this.createFakeStudents(100);
+
+        mockMvc.perform(get("/student?page=0&pageSize=4"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content", hasSize(4)))
+                .andExpect(jsonPath("$.content[0].name", is("name 0")))
+                .andExpect(jsonPath("$.content[0].email", is("email 0")))
+                .andExpect(jsonPath("$.content[0].linkedin", is("linkedin 0")))
+                .andExpect(jsonPath("$.content[0].university", is("UNI 0")))
+                .andExpect(jsonPath("$.content[0].graduation", is("Grad 0")))
+                .andExpect(jsonPath("$.content[1].name", is("name 1")))
+                .andExpect(jsonPath("$.content[1].email", is("email 1")))
+                .andExpect(jsonPath("$.content[1].linkedin", is("linkedin 1")))
+                .andExpect(jsonPath("$.content[1].university", is("UNI 1")))
+                .andExpect(jsonPath("$.content[1].graduation", is("Grad 1")))
+                .andExpect(jsonPath("$.content[2].name", is("name 10")))
+                .andExpect(jsonPath("$.content[2].email", is("email 10")))
+                .andExpect(jsonPath("$.content[2].linkedin", is("linkedin 10")))
+                .andExpect(jsonPath("$.content[2].university", is("UNI 10")))
+                .andExpect(jsonPath("$.content[2].graduation", is("Grad 10")))
+                .andExpect(jsonPath("$.content[3].name", is("name 11")))
+                .andExpect(jsonPath("$.content[3].email", is("email 11")))
+                .andExpect(jsonPath("$.content[3].linkedin", is("linkedin 11")))
+                .andExpect(jsonPath("$.content[3].university", is("UNI 11")))
+                .andExpect(jsonPath("$.content[3].graduation", is("Grad 11")));
+
+    }
+
+    @Test
+    void whenRequestingStudentListAndPageQueryParamIsInvalid_thenReturnBadRequestError() throws Exception {
+        mockMvc.perform(get("/student?page=xx&pageSize=4"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.details").isArray())
+                .andExpect(jsonPath("$.details", hasSize(1)));
+    }
+
+    @Test
+    void whenRequestingStudentListAndPageQueryParamIsMissing_thenReturnBadRequestError() throws Exception {
+        mockMvc.perform(get("/student?pageSize=4"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.details").isArray())
+                .andExpect(jsonPath("$.details", hasSize(1)));
+    }
+
+    @Test
+    void whenRequestingStudentListAndPageSizeQueryParamIsInvalid_thenReturnBadRequestError() throws Exception {
+        mockMvc.perform(get("/student?pageSize=xx&page=4"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.details").isArray())
+                .andExpect(jsonPath("$.details", hasSize(1)));
+    }
+
+    @Test
+    void whenRequestingStudentListAndPageSizeQueryParamIsMissing_thenReturnBadRequestError() throws Exception {
+        mockMvc.perform(get("/student?page=0"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.details").isArray())
+                .andExpect(jsonPath("$.details", hasSize(1)));
+    }
+
+    private void createFakeStudents(int amount) {
+        for (int i = 0; i < amount; i++) {
+            User u = new User();
+            u.setEmail("email " + i);
+            u.setName("name " + i);
+            u.setLinkedin("linkedin " + i);
+            u.setPassword("pwd " + i);
+
+            Student student = Student.builder()
+                    .university("UNI " + i)
+                    .graduation("Grad " + i)
+                    .finishDate(LocalDate.now())
+                    .user(u)
+                    .build();
+
+            this.studentRepository.save(student);
+        }
+    }
+
+}
+
---
+++ deamanagement/src/test/java/br/com/dea/management/student/StudentGetByIdTests.java
@@ -0,0 +1,110 @@
+package br.com.dea.management.student;
+
+import br.com.dea.management.student.domain.Student;
+import br.com.dea.management.student.repository.StudentRepository;
+import br.com.dea.management.user.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Slf4j
+public class StudentGetByIdTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        log.info("Before each test in " + StudentGetByIdTests.class.getSimpleName());
+    }
+
+    @BeforeAll
+    void beforeSuiteTest() {
+        log.info("Before all tests in " + StudentGetByIdTests.class.getSimpleName());
+    }
+
+    @Test
+    void whenRequestingAnExistentStudentById_thenReturnTheStudentSuccessfully() throws Exception {
+        this.studentRepository.deleteAll();
+        this.createFakeStudents(10);
+
+        Student student = this.studentRepository.findAll().get(0);
+
+        mockMvc.perform(get("/student/" + student.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name", is(student.getUser().getName())))
+                .andExpect(jsonPath("$.email", is(student.getUser().getEmail())))
+                .andExpect(jsonPath("$.linkedin", is(student.getUser().getLinkedin())))
+                .andExpect(jsonPath("$.university", is(student.getUniversity())))
+                .andExpect(jsonPath("$.graduation", is(student.getGraduation())));
+
+    }
+
+    @Test
+    void whenRequestingByIdAndIdIsNotANumber_thenReturnTheBadRequestError() throws Exception {
+
+        mockMvc.perform(get("/student/xx"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.details").isArray())
+                .andExpect(jsonPath("$.details", hasSize(1)));
+    }
+
+    @Test
+    void whenRequestingAnNonExistentStudentById_thenReturnTheNotFoundError() throws Exception {
+
+        mockMvc.perform(get("/student/5000"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.details").isArray())
+                .andExpect(jsonPath("$.details", hasSize(1)));
+    }
+
+    private void createFakeStudents(int amount) {
+        for (int i = 0; i < amount; i++) {
+            User u = new User();
+            u.setEmail("email " + i);
+            u.setName("name " + i);
+            u.setLinkedin("linkedin " + i);
+            u.setPassword("pwd " + i);
+
+            Student student = Student.builder()
+                    .university("UNI " + i)
+                    .graduation("Grad " + i)
+                    .finishDate(LocalDate.now())
+                    .user(u)
+                    .id(Long.parseLong(String.valueOf(i)))
+                    .build();
+
+            this.studentRepository.save(student);
+        }
+        List<Student> s = this.studentRepository.findAll();
+    }
+
+}

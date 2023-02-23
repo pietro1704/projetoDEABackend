96a40de3d8327127c9aebca654bc0888e9e42955
Pietro Pugliesi <pietropugliesi@icloud.com>
2023-02-23 16:07:01 -0300

    semana5: swagger implementation

--- deamanagement/src/main/java/br/com/dea/management/DeamanagementApplication.java
+++ deamanagement/src/main/java/br/com/dea/management/DeamanagementApplication.java
@@ -5,6 +5,9 @@ import br.com.dea.management.student.repository.StudentRepository;
 import br.com.dea.management.user.domain.User;
 import br.com.dea.management.user.repository.UserRepository;
 import br.com.dea.management.user.service.UserService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.time.LocalDate;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Dea Management", version = "1.0", description = "Dea Management API Description"),
+		servers = {
+				@Server(url = "http://localhost:8082${server.servlet.contextPath}", description = "Local environment URL"),
+				@Server(url = "https://deamanagement.com.br${server.servlet.contextPath}", description = "Development environment URL")
+		}
+)
+
 public class DeamanagementApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
--- deamanagement/src/main/java/br/com/dea/management/student/controller/StudentController.java
+++ deamanagement/src/main/java/br/com/dea/management/student/controller/StudentController.java
@@ -3,6 +3,10 @@ package br.com.dea.management.student.controller;
 import br.com.dea.management.student.domain.Student;
 import br.com.dea.management.student.dto.StudentDto;
 import br.com.dea.management.student.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,22 +16,41 @@ import java.util.List;
 
 @RestController
 @Slf4j
+@Tag(name = "Student", description = "The Student API")
 public class StudentController {
 
     @Autowired
     StudentService studentService;
 
+    @Operation(summary = "Load the list of students with all attributes.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "500", description = "Error fetching student list"),
+    })
+    @Deprecated
     @RequestMapping(value = "/student/all", method = RequestMethod.GET)
     public List<Student> getStudentsAllRaw() {
         return this.studentService.findAllStudents();
     }
 
+    @Operation(summary = "Load all students.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "500", description = "Error fetching student list"),
+    })
+    @Deprecated
     @RequestMapping(value = "/student/without-pagination", method = RequestMethod.GET)
     public List<StudentDto> getStudentsWithOutPagination() {
         List<Student> students = this.studentService.findAllStudents();
         return StudentDto.fromStudents(students);
     }
 
+    @Operation(summary = "Load the list of students paginated.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Page or Page Size params not valid"),
+            @ApiResponse(responseCode = "500", description = "Error fetching student list"),
+    })
     @GetMapping("/student")
     public Page<StudentDto> getStudents(@RequestParam(required = true) Integer page,
                                         @RequestParam(required = true) Integer pageSize) {
@@ -43,6 +66,13 @@ public class StudentController {
 
     }
 
+    @Operation(summary = "Load the student by ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Student Id invalid"),
+            @ApiResponse(responseCode = "404", description = "Student Not found"),
+            @ApiResponse(responseCode = "500", description = "Error fetching student list"),
+    })
     @GetMapping("/student/{id}")
     public StudentDto getStudents(@PathVariable Long id) {
 
